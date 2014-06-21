38a0d87b2a23812fdbf032031426d27ba50eb21e
paulbatley <paulbatley@users.noreply.github.com>
2014-06-20 19:06:38 -0700

    Update dungeonHeader.h

--- dungeonHeader.h
+++ dungeonHeader.h
@@ -12,26 +12,29 @@ class DuGame
 {
 	private:
 		BoardElements Board[ROWS][COLS];
+		BoardElements PrevObj;
 		int playersNum;
 		int r1,c1;
 		int cash;
 		int rb1,cb1;
+		int rp1, cp1;
 		int level;
 		int lives;
 		char BoardElementsToChars(BoardElements b) const;
 	public:
 		DuGame(int numOfPlayers = 1);
-		void printBoard();
 		void checkOOB();
-		void updateBoard();
+		int updateBoard();
 		void startPlaying(char userMove);
 		void nextLevel();
 		void levels(int levelSelected);
 		void resetBoard();
 		friend std::ostream & operator<<(std::ostream & os, const DuGame d);
 		int checkLives();
-		
+		int checkForExit();
 		int checkLevel();
+		void displayPlayerStatus() const;
+		void prevCoor();
 };
 
 #endif
